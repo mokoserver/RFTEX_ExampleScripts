6161190548de1cedcbac1eea9d270fa4bc6285c2
mokoserver <mokoserver@gmail.com>
2026-05-25 23:14:12 +0300

    первое обновление

--- 01 Example project function/En 03 Graph function/6 Farewell.py
+++
@@ -1,7 +0,0 @@
-import RFSE
-
-RFSE.Plugin("Graph", 'close', '')
-
-RFSE.Messenger('set', 'Farewell#@bye', 'You have looked at the work of the Graph plugin. Good luck!')
-
-RFSE.EndScript()
--- 01 Example project function/En 04 Messenger function/6 Farewell.py
+++
@@ -1,5 +0,0 @@
-import RFSE
-
-RFSE.Messenger('set', 'Farewell#@bye', 'That\'s all! Good luck!')
-
-RFSE.EndScript()
